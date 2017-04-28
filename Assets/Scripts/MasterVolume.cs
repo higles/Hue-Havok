489da7cd412529242fe1ca058e8e4a410bcc8656
Joe Higley <josephhigley@gmail.com>
2017-04-28 11:25:22 -0700

    Added functional styling to items on options page

--- Assets/Scripts/MasterVolume.cs
+++ Assets/Scripts/MasterVolume.cs
@@ -6,8 +6,12 @@ public class MasterVolume : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-	}
+        float ratio = (255f - 100f) / 255f;
+        float r = (255f / 255f) - (transform.GetComponent<Slider>().value * ratio);
+        float g = (100f / 255f) + (transform.GetComponent<Slider>().value * ratio);
+
+        transform.FindChild("Fill Area").FindChild("Fill").GetComponent<Image>().color = new Color(r, g, (100f / 255f));
+    }
 	
 	// Update is called once per frame
 	void Update () {
